b7116c7fcf894bcd3676542c5db0805950f95226
shiv <shivprataps@gmail.com>
2018-05-10 15:03:25 +0530

    build updated, kafka stream fixes

--- src/main/kotlin/com/und/config/EventStream.kt
+++ src/main/kotlin/com/und/config/EventStream.kt
@@ -1,29 +1,12 @@
 package com.und.config
 
-import org.springframework.cloud.stream.annotation.Input
 import org.springframework.cloud.stream.annotation.Output
 import org.springframework.messaging.MessageChannel
-import org.springframework.messaging.SubscribableChannel
 
 
 interface EventStream {
 
-//    @Input("emailEvent")
-//    fun emailEventSend(): SubscribableChannel
-
-//    @Input("campaignTrigger")
-//    fun campaignTriggerSend(): SubscribableChannel
-
-    @Output("clientEmail")
+    @Output("clientEmailSend")
     fun clientEmailSend(): MessageChannel
 
-//    @Input("EmailUpdate")
-//    fun emailUpdateEvent(): SubscribableChannel
-//
-//    @Input("clickTrackEvent")
-//    fun clickTrackEvent(): SubscribableChannel
-
-
-//    @Output("emailEvent")
-//    fun outputEmailEvent(): MessageChannel
 }
